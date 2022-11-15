40326c14065d011d6d2addd89d12f594eaf0658c
Junkai <junkai2@illinois.edu>
2022-11-15 09:39:20 -0500

    gitignore

--- text2unit_fairseq/scripts/eval_t2u.sh
+++ text2unit_fairseq/scripts/eval_t2u.sh
@@ -16,7 +16,7 @@ srun --gres=gpu:1 --ntasks=1 --mem=200G -c 16 fairseq-generate ${t2u_dir}/data-b
     --path ${project_dir}/checkpoint${epoch}.pt --gen-subset ${gen_subset} --batch-size 120 --beam ${beam} \
     --max-len-a 20 --max-len-b 5 --scoring wer --fp16 | tee ${project_dir}/units_epoch${epoch}_${gen_subset}_beam${beam}.txt
 
-srun --gres=gpu:1 --ntasks=1 --mem=200G -c 16 ${t2u_dir}/unit_error_rate.py \
+srun --gres=gpu:1 --ntasks=1 --mem=200G -c 16 python ${t2u_dir}/unit_error_rate.py \
     --epoch ${epoch} \
     --eval_path ${project_dir}/units_epoch${epoch}_${gen_subset}_beam${beam}.txt \
     --write_path ${project_dir}/${gen_subset}_uer.txt
