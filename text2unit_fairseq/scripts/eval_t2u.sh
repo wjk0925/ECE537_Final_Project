243c521d1f6e680d41c941934068cd55e62dbeba
Junkai <junkai2@illinois.edu>
2022-11-15 03:40:17 -0600

    chmod

--- text2unit_fairseq/scripts/eval_t2u.sh
+++ text2unit_fairseq/scripts/eval_t2u.sh
@@ -15,14 +15,16 @@ set -o pipefail
 source /nobackup/users/heting/espnet/tools/conda/bin/../etc/profile.d/conda.sh
 conda activate babblenet # change it to your conda environment
 
-srun --gres=gpu:1 --ntasks=1 --mem=200G -c 16 fairseq-generate ${t2u_dir}/data-bin/${dataset} \
+#srun --gres=gpu:1 --ntasks=1 --mem=200G -c 16 
+fairseq-generate ${t2u_dir}/data-bin/${dataset} \
     --path ${project_dir}/${epoch} --gen-subset ${gen_subset} --batch-size 128 --beam ${beam} \
     --max-len-a 20 --max-len-b 5 | tee ${project_dir}/units_epoch${epoch}_${gen_subset}_beam${beam}.txt
     
 source /nobackup/users/junkaiwu/anaconda3/etc/profile.d/conda.sh
 conda activate diffusion
 
-srun --gres=gpu:1 --ntasks=1 --mem=200G -c 16 ${t2u_dir}/unit_error_rate.py \
+srun --gres=gpu:1 --ntasks=1 --mem=200G -c 16 
+#${t2u_dir}/unit_error_rate.py \
     --epoch ${epoch} \
     --eval_path ${project_dir}/units_epoch${epoch}_${gen_subset}_beam${beam}.txt \
     --write_path ${project_dir}/${gen_subset}_uer.txt
