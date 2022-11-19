f7ad705608db2133a17ced5d1f2271593e40565b
Junkai <junkai2@illinois.edu>
2022-11-19 04:34:05 -0500

    nisqa

--- nnmos/nisqa.sh
+++ nnmos/nisqa.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 nisqa_dir="/u/junkaiwu/NISQA"
-data_dirs=( "/u/junkaiwu/speech-resynthesis/generations/lj_hubert200_004600000" "/u/junkaiwu/speech-resynthesis/generations/lj_hubert100_00500000_released" )
+data_dir="/nobackup/users/junkaiwu/outputs/hubert_hifigan_unit2speech/test100_reconstruct_500000released"
 . parse_options.sh || exit 1;
-
-for i in ${!data_dirs[@]}; do
-    data_dir=${data_dirs[$i]}
     
-    python ${nisqa_dir}/run_predict.py --mode predict_dir --pretrained_model ${nisqa_dir}/weights/nisqa_tts.tar --data_dir ${data_dir} --num_workers 0 --bs 10 --output_dir ${data_dir}
+python ${nisqa_dir}/run_predict.py --mode predict_dir --pretrained_model ${nisqa_dir}/weights/nisqa_tts.tar --data_dir ${data_dir} --num_workers 0 --bs 10 --output_dir ${data_dir}
 
-    mv ${data_dir}/NISQA_results.csv ${data_dir}/NISQA_TTS_results.csv
+mv ${data_dir}/NISQA_results.csv ${data_dir}/NISQA_TTS_results.csv
 
-    python ${nisqa_dir}/run_predict.py --mode predict_dir --pretrained_model ${nisqa_dir}/weights/nisqa.tar --data_dir ${data_dir} --num_workers 0 --bs 10 --output_dir ${data_dir}
-    
-done
+python ${nisqa_dir}/run_predict.py --mode predict_dir --pretrained_model ${nisqa_dir}/weights/nisqa.tar --data_dir ${data_dir} --num_workers 0 --bs 10 --output_dir ${data_dir}
 
