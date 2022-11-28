bd9054d96cb8f4362b87dc4dbd5b2dc59bdcfd1f
Junkai <junkai2@illinois.edu>
2022-11-28 16:54:26 +0800

    m

--- asr/fairseq/wav2vec/asr.slurm.sh
+++ asr/fairseq/wav2vec/asr.slurm.sh
@@ -50,10 +50,10 @@ wav2vec_path="/home/junkaiwu/workspace/ulm_eval/models/asr/wav2vec_big_960h.pt"
 lm_path="/home/junkaiwu/workspace/ulm_eval/models/asr/4-gram.bin"
 lexicon_path="/home/junkaiwu/workspace/ulm_eval/models/asr/lexicon_ltr.lst"
 
-roots=( "/home/junkaiwu/ECE537_Final_Project/kan-bayashi/ljspeech_conformer_fastspeech2" "/home/junkaiwu/ECE537_Final_Project/kan-bayashi/ljspeech_fastspeech2" "/home/junkaiwu/ECE537_Final_Project/kan-bayashi/ljspeech_joint_finetune_conformer_fastspeech2_hifigan" "/home/junkaiwu/ECE537_Final_Project/kan-bayashi/ljspeech_tacotron2" "/home/junkaiwu/ECE537_Final_Project/kan-bayashi/ljspeech_vits" )
+roots=( "/home/junkaiwu/ECE537_Final_Project/text2unit/t2u_outputs/hubert200_v2_libritts/95_preds_tacotron/16k" "/home/junkaiwu/ECE537_Final_Project/text2unit/t2u_outputs/hubert200_v2_libritts/95_preds_hifigan" )
 
 nnmos_dir="/home/junkaiwu/ECE537_Final_Project/nnmos"
-transcription_path="/home/junkaiwu/ECE537_Final_Project/datasets/LJSpeech/ljspeech.json"
+transcription_path="/home/junkaiwu/ECE537_Final_Project/datasets/LibriTTS_train-clean-100/libritts.json"
 
 for i in ${!roots[@]}; do
     root=${roots[$i]}
