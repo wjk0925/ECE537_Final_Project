6defb37f3a578e6b38fb88631b9b7f2d9c1479e4
Junkai <junkai2@illinois.edu>
2022-11-29 17:01:13 +0800

    m

--- text2unit/scripts/train_hubert200_libritts.slurm.sh
+++ text2unit/scripts/train_hubert200_libritts.slurm.sh
@@ -62,7 +62,7 @@ text2unit_dir="/home/junkaiwu/ECE537_Final_Project/text2unit"
 train_txt_path="/home/junkaiwu/ECE537_Final_Project/datasets/LibriTTS_train-clean-100/hubert/train200.txt"
 val_txt_path="/home/junkaiwu/ECE537_Final_Project/datasets/LibriTTS_train-clean-100/hubert/val200.txt"
 trg_vocab_size=203
-exp_name="hubert200_libritts_emb${}_heads${}_layers${}_batch${train_batch_size}_warm${warmup_steps}"
+exp_name="hubert200_libritts_emb${embedding_dim}_heads${num_heads}_layers${num_layers}_batch${train_batch_size}_warm${warmup_steps}"
 
 srun --gres=gpu:1 --ntasks=1  python ${text2unit_dir}/train_v2.py \
     --embedding_dim ${embedding_dim}  \
