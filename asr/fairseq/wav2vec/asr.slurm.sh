2b924878543a9caa69be1e18b4495a127d75b855
Junkai <junkai2@illinois.edu>
2022-12-01 02:47:07 +0800

    m

--- asr/fairseq/wav2vec/asr.slurm.sh
+++ asr/fairseq/wav2vec/asr.slurm.sh
@@ -50,11 +50,11 @@ wav2vec_path="/home/junkaiwu/workspace/ulm_eval/models/asr/wav2vec_big_960h.pt"
 lm_path="/home/junkaiwu/workspace/ulm_eval/models/asr/4-gram.bin"
 lexicon_path="/home/junkaiwu/workspace/ulm_eval/models/asr/lexicon_ltr.lst"
 
-roots=( "/home/junkaiwu/ECE537_Final_Project/text2unit/t2u_outputs/hubert200_ljspeech_emb512_heads8_layers6_batch32_warm8000/ljspeech_265_hifigan" "/home/junkaiwu/ECE537_Final_Project/text2unit/t2u_outputs/hubert200_libritts_emb512_heads8_layers6_batch32_warm8000/ljspeech_205_hifigan" )
+roots=( "/home/junkaiwu/ECE537_Final_Project/text2unit_fairseq/outputs/transformer_iwslt_de_en-dataset_ljspeech_hubert200-dropout_0.3-max_tokens_4096-share/units_best_ckpt_1_test_beam7_u2s_hifigan_2" "/home/junkaiwu/ECE537_Final_Project/text2unit_fairseq/outputs/transformer_iwslt_de_en-dataset_ljspeech_hubert100-dropout_0.3-max_tokens_4096-share/units_best_ckpt_1_test_beam7_u2s_hifigan_2" )
 
 nnmos_dir="/home/junkaiwu/ECE537_Final_Project/nnmos"
-#transcription_path="/home/junkaiwu/ECE537_Final_Project/datasets/LibriTTS_train-clean-100/libritts.json"
-transcription_path="/home/junkaiwu/ECE537_Final_Project/datasets/LJSpeech/ljspeech.json"
+transcription_path="/home/junkaiwu/ECE537_Final_Project/datasets/LibriTTS_train-clean-100/libritts.json"
+# transcription_path="/home/junkaiwu/ECE537_Final_Project/datasets/LJSpeech/ljspeech.json"
 
 for i in ${!roots[@]}; do
     root=${roots[$i]}
