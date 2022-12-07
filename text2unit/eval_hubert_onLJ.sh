72921f90f1da2d5d4dc678d48eac8182ac258e34
Junkai <junkai2@illinois.edu>
2022-12-07 17:04:03 +0800

    m

--- text2unit/eval_hubert_onLJ.sh
+++ text2unit/eval_hubert_onLJ.sh
@@ -8,15 +8,18 @@ for (( epoch=0; epoch<=295; epoch+=5 ))
 do
     output_name=${exp_dir}/ljspeech_${split}${epoch}.km
 
-    python eval_v2.py \
-        --vocab_size ${vocab_size} \
-        --exp_dir ${exp_dir} \
-        --test_txt_path /home/junkaiwu/ECE537_Final_Project/datasets/LJSpeech/hubert/${split}${vocab_size}.txt \
-        --num_workers 16 \
-        --batch_size 60 \
-        --epoch ${epoch} \
-        --output_name ${output_name}
-    
+    if [ -f "$output_name" ]; then
+        echo "exist!"
+    else
+        python eval_v2.py \
+            --vocab_size ${vocab_size} \
+            --exp_dir ${exp_dir} \
+            --test_txt_path /home/junkaiwu/ECE537_Final_Project/datasets/LJSpeech/hubert/${split}${vocab_size}.txt \
+            --num_workers 16 \
+            --batch_size 60 \
+            --epoch ${epoch} \
+            --output_name ${output_name}
+    fi
 done
 
 
