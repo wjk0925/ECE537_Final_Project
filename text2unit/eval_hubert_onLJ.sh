b663a887ebeb9a19c8f35812a9fd56dd6b28034c
Junkai <junkai2@illinois.edu>
2022-12-07 17:02:08 +0800

    m

--- text2unit/eval_hubert_onLJ.sh
+++ text2unit/eval_hubert_onLJ.sh
@@ -3,10 +3,9 @@ text2unit_dir="/home/junkaiwu/ECE537_Final_Project/text2unit"
 vocab_size=200
 exp_dir="/home/junkaiwu/ECE537_Final_Project/text2unit/t2u_outputs/hubert200_ljspeech_emb512_heads8_layers6_batch64_warm4000"
 split="val"
-epochs=( 290 )
 
-for i in ${!epochs[@]}; do
-    epoch=${epochs[$i]}
+for (( epoch=0; epoch<=295; epoch+=5 ))
+do
     output_name=${exp_dir}/ljspeech_${split}${epoch}.km
 
     python eval_v2.py \
